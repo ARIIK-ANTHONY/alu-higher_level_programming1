8851e5885f50252b645fde2e4af9cba302f4cdfb
SLICKMAN-TYRUS <a.chol@alustudent.com>
2024-10-24 08:25:29 -0700

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        if i == 0 and j == 0:
-            continue
-        print(f"{i}{j}", end=', ' if (i, j) != (8, 9) else '\n')
+for x in range(0, 10):
+    for y in range(x + 1, 10):
+        if x == 8 and y == 9:
+            print('89')
+        else:
+            print('{}{}, '.format(x, y), end='')
